957e5ca8ff25417c13eb2fa64c3272d78222cf0e
Danushri Prakash <danushri.saranyaprakash@gmail.com>
2025-08-05 14:46:57 +0530

    Day-2

--- Backend/database/db.js
+++ Backend/database/db.js
@@ -1,12 +1,14 @@
-
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI); 
-    console.log("Connected to MongoDB");
-  } catch (error) {
-    console.error("MongoDB connection error:", error);
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('✅ MongoDB connected');
+  } catch (err) {
+    console.error('❌ MongoDB connection failed:', err.message);
     process.exit(1);
   }
 };
